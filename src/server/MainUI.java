bb434f19976b31b3b2bf5804df44e10eb7a7ba06
karimshannawaz <karimshannawaz@my.unt.edu>
2020-04-13 18:45:13 -0500

    Shan - Adjusted MainUI to be dynamic

--- src/server/MainUI.java
+++ src/server/MainUI.java
@@ -33,7 +33,7 @@ public class MainUI extends JFrame {
 
 	private static final long serialVersionUID = 4680811856634328038L;
 	
-	private JPanel UtilityPanel;
+	private JPanel utilityPanel;
 	
 	public static long onlineTime;
 
@@ -41,6 +41,7 @@ public class MainUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MainUI() {
+		
 		CoresManager.slowExecutor.scheduleWithFixedDelay(new Runnable() {
 			@Override
 			public void run() {
@@ -52,271 +53,59 @@ public class MainUI extends JFrame {
 				}
 			}
 		}, 0, 1, TimeUnit.SECONDS);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setBounds(100, 100, 1172, 761);
 		
 		//Create Frame to hold Manager Tasks
-		UtilityPanel = new JPanel();
-		UtilityPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(UtilityPanel);
-		UtilityPanel.setLayout(null);
+		utilityPanel = new JPanel();
+		utilityPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(utilityPanel);
+		utilityPanel.setLayout(null);
 		
-		JToggleButton ClockInOut_btn = new JToggleButton("Clock In/Out");
-		ClockInOut_btn.setBounds(0, -3, 232, 74);
-		UtilityPanel.add(ClockInOut_btn);
+		String[] panelNames = { 
+			"Clock In/Out",
+			"Order",
+			"Pay",
+			"Compensate",
+			"Tables",
+			"Inventory",
+			"Menu",
+			"Time Log",
+			"Discounts",
+			"Employees"
+		};
 		
-		JToggleButton Order_btn = new JToggleButton("Order");
-		Order_btn.setBounds(0, 68, 232, 74);
-		UtilityPanel.add(Order_btn);
+		JToggleButton[] panelBtns = new JToggleButton[panelNames.length];
 		
-		JToggleButton Pay_btn = new JToggleButton("Pay");
-		Pay_btn.setBounds(0, 139, 232, 74);
-		UtilityPanel.add(Pay_btn);
+		for(int index = 0; index < panelBtns.length; index++) {
+			panelBtns[index] = new JToggleButton(panelNames[index]);
+			panelBtns[index].setBounds(0, (71 * index) - 3, 232, 74);
+			utilityPanel.add(panelBtns[index]);
+		}
 		
-		JToggleButton Compensate_btn = new JToggleButton("Compensate");
-		Compensate_btn.setBounds(0, 210, 232, 74);
-		UtilityPanel.add(Compensate_btn);
-		
-		JToggleButton Tables_btn = new JToggleButton("Tables");
-		Tables_btn.setBounds(0, 281, 232, 74);
-		UtilityPanel.add(Tables_btn);
-		
-		JToggleButton Inventory_btn = new JToggleButton("Inventory");
-		Inventory_btn.setBounds(0, 352, 232, 74);
-		UtilityPanel.add(Inventory_btn);
-		
-		JButton AddItem_btn = new JButton("Add Menu Item");
-		AddItem_btn.setBounds(0, 423, 232, 74);
-		AddItem_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				addMenuItem();
-			}
-		});
-		UtilityPanel.add(AddItem_btn);
-		
-		JToggleButton TimeLog_btn = new JToggleButton("Time Log");
-		TimeLog_btn.setBounds(0, 494, 232, 74);
-		UtilityPanel.add(TimeLog_btn);
-		
-		JToggleButton Discounts_btn = new JToggleButton("Discounts");
-		Discounts_btn.setBounds(0, 565, 232, 74);
-		UtilityPanel.add(Discounts_btn);
-		
-		JToggleButton Employees_btn = new JToggleButton("Employees");
-		Employees_btn.setBounds(0, 636, 232, 74);
-		UtilityPanel.add(Employees_btn);
-		
-		/*
-		ClockInOut_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (ClockInOut_btn.isSelected())
-				{
+		for(int index = 0; index < panelBtns.length; index++) {
+			JToggleButton btn = panelBtns[index];
+			btn.addActionListener(new ActionListener() {
 
-					Order_btn.setSelected(false);
-					Pay_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					Employees_btn.setSelected(false);
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if(btn.isSelected()) {
+						for(int i = 0; i < panelBtns.length; i++) {
+							if(btn.getText().equals(panelBtns[i].getText())) {
+								continue;
+							}
+							panelBtns[i].setSelected(false);
+						}
+					}
+					else {
+						btn.setSelected(false);
+					}
 				}
-				else
-				{
-					ClockInOut_btn.setSelected(false);
-				}
-			}
-		});
-		Order_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (Order_btn.isSelected())
-				{
-					Pay_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					Employees_btn.setSelected(false);
-					ClockInOut_btn.setSelected(false);
-				}
-				else
-				{
-					Order_btn.setSelected(false);
-				}
-			}
-		});
-		Pay_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (Pay_btn.isSelected())
-				{
-
-					Order_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					Employees_btn.setSelected(false);
-					ClockInOut_btn.setSelected(false);
-				}
-				else
-				{
-					Pay_btn.setSelected(false);
-				}
-			}
-		});
-		Compensate_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (Compensate_btn.isSelected())
-				{
-
-					Order_btn.setSelected(false);
-					Pay_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					Employees_btn.setSelected(false);
-					ClockInOut_btn.setSelected(false);
-				}
-				else
-				{
-					Compensate_btn.setSelected(false);
-				}
-			}
-		});
-		Tables_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (Tables_btn.isSelected())
-				{
-
-					Order_btn.setSelected(false);
-					Pay_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					Employees_btn.setSelected(false);
-					ClockInOut_btn.setSelected(false);
-				}
-				else
-				{
-					Tables_btn.setSelected(false);
-				}
-			}
-		});
-		Inventory_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (Inventory_btn.isSelected())
-				{
-
-					Order_btn.setSelected(false);
-					Pay_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					Employees_btn.setSelected(false);
-					ClockInOut_btn.setSelected(false);
-				}
-				else
-				{
-					Inventory_btn.setSelected(false);
-				}
-			}
-		});
-		AddItem_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (AddItem_btn.isSelected())
-				{
-					ClockInOut_btn.setSelected(false);
-					Order_btn.setSelected(false);
-					Pay_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					Employees_btn.setSelected(false);
-				}
-				else
-				{
-					AddItem_btn.setSelected(false);
-				}
-			}
-		});
-		TimeLog_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (TimeLog_btn.isSelected())
-				{
-
-					Order_btn.setSelected(false);
-					Pay_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					Employees_btn.setSelected(false);
-					ClockInOut_btn.setSelected(false);
-				}
-				else
-				{
-					TimeLog_btn.setSelected(false);
-				}
-			}
-		});
-		Discounts_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (Discounts_btn.isSelected())
-				{
-
-					Order_btn.setSelected(false);
-					Pay_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					ClockInOut_btn.setSelected(false);
-					Employees_btn.setSelected(false);
-				}
-				else
-				{
-					Discounts_btn.setSelected(false);
-				}
-			}
-		});
-		Employees_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (Employees_btn.isSelected())
-				{
-
-					Order_btn.setSelected(false);
-					Pay_btn.setSelected(false);
-					Compensate_btn.setSelected(false);
-					Tables_btn.setSelected(false);
-					Inventory_btn.setSelected(false);
-					AddItem_btn.setSelected(false);
-					TimeLog_btn.setSelected(false);
-					Discounts_btn.setSelected(false);
-					ClockInOut_btn.setSelected(false);
-				}
-				else
-				{
-					Employees_btn.setSelected(false);
-				}
-			}
-		});
-		*/
+				
+			});
+		}
 		
 	}
 
