8b448e251e20ea098c0cdb4bf767833d154585c4
ashaw <ashaw@DESKTOP-UMJ1B2A.home>
2020-04-15 13:35:56 -0500

    Completed?? InventoryPanelTest

--- JUnitTests/server/ui/InventoryPanelTest.java
+++ JUnitTests/server/ui/InventoryPanelTest.java
@@ -4,16 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import server.menu.Inventory;
+
+import java.awt.Font;
+import java.awt.Font;
 class InventoryPanelTest {
 
 	@Test
 	void testAddIngredient() {
-		fail("Not yet implemented"); 
+		//fail("Not yet implemented"); 
+		Inventory.instance.put("swiss",500);
+		assertTrue(Inventory.instance.containsKey("swiss"));
+		assertEquals(500, Inventory.instance.get("swiss"));
 	}
 
 	@Test
 	void testUpdateInventory() {
-		fail("Not yet implemented"); 
+		//fail("Not yet implemented"); 
+		Integer name = Inventory.instance.get("cheese");
+		Inventory.instance.put("cheese", 10);
+		assertNotEquals(name, Inventory.instance.get("cheese"));
 	}
 
 }
