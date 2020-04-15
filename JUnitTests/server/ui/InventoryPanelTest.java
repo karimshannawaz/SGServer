3ba220adc578b34b524fa31ec99b0348624b91f2
ashaw <ashaw@192.168.1.220>
2020-04-15 09:47:55 -0500

    added JUnit test folder for Server side

---
+++ JUnitTests/server/ui/InventoryPanelTest.java
@@ -0,0 +1,19 @@
+package server.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InventoryPanelTest {
+
+	@Test
+	void testAddIngredient() {
+		fail("Not yet implemented"); 
+	}
+
+	@Test
+	void testUpdateInventory() {
+		fail("Not yet implemented"); 
+	}
+
+}
---
+++ JUnitTests/server/ui/MenuPanelTest.java
@@ -0,0 +1,19 @@
+package server.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MenuPanelTest {
+
+	@Test
+	void testUpdateItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddToMenu() {
+		fail("Not yet implemented");
+	}
+
+}
