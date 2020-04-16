b5b1121c825cb14d39ca42235f5daf5025b275be
ashaw <ashaw@192.168.1.220>
2020-04-16 15:10:47 -0500

    added setups for junit tests

---
+++ JUnitTests/server/menu/InventoryTest.java
@@ -0,0 +1,19 @@
+package server.menu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InventoryTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testLoadInventory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ JUnitTests/server/ui/EmployeePanelTest.java
@@ -0,0 +1,29 @@
+package server.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmployeePanelTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testEmployeePanel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddEmployee() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testRemoveEmployee() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
