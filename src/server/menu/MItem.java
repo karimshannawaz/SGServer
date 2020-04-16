f8f284820837c11adc414aeb28e7f8f5158ddbaa
karimshannawaz <karimshannawaz@my.unt.edu>
2020-04-16 13:05:35 -0500

    menu update

--- src/server/menu/MItem.java
+++ src/server/menu/MItem.java
@@ -1,8 +1,5 @@
 package server.menu;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Represents a single item on the menu with its respective attributes. 
  * SERVER SIDED
@@ -19,7 +16,6 @@ public class MItem {
 	public String allergens;
 	public String type; // indicates if the menu item is vegan, vegetarian or neither (default).
 	public String menuType; // indicates if the item is an entree, drink, dessert or side.
-	//public List<Ingredient> ingredients; // Represents a list of ingredients for this menu item
 	public String ingredients;
 	
 	// Helps with order totals; the rest of the variables: sub, specialReqs
@@ -37,7 +33,6 @@ public class MItem {
 		this.allergens = allergens;
 		this.type = type;
 		this.menuType = menuType;
-		//this.ingredients = new ArrayList<Ingredient>();
 		this.ingredients = ingredients;
 	}
 
@@ -49,24 +44,9 @@ public class MItem {
 		this.allergens = "none";
 		this.type = "default";
 		this.menuType = "entree";
-		//this.ingredients = new ArrayList<Ingredient>();
 		this.ingredients = null;
 	}
 	
-	/**
-	 * Adds an ingredient to this menu item.
-	 * @param name
-	 * @param qty
-	 * @param editable
-	 * @param substitutions
-	 
-	public void addIng(String name, int qty, boolean editable, String substitutions) {
-		Ingredient ing = new Ingredient(name, qty, editable);
-		ing.addSub(substitutions);
-		ingredients.add(ing);
-	}
-	*/
-
 	@Override
 	public String toString() {
 		return name + "~" + price + "~" + description + "~" + calories + "~" + allergens + "~" + type
@@ -81,19 +61,6 @@ public class MItem {
 		return name+"~"+price+"~"+qty+"~"+specialReqs+"~"+ingredients;
 	}
 
-	/**
-	 * Returns ingredients to string.
-	 * @return
-	 
-	public String getIngsToString() {
-		String output = "";
-		for(int i = 0; i < ingredients.size(); i++) {
-			output += ingredients.get(i).toString();
-			if(i < ingredients.size() - 1)
-				output += ",";
-		}
-		return output;
-	}
-	*/
+
 
 }
