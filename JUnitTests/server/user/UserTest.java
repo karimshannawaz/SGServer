e94a59df83ef44de828b085caa77ed46a9b458e6
ashaw <ashaw@192.168.1.220>
2020-04-17 16:53:20 -0500

    adding test cases

--- JUnitTests/server/user/UserTest.java
+++ JUnitTests/server/user/UserTest.java
@@ -9,181 +9,193 @@ class UserTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		User tester = new User();
 	}
 
 	@Test
 	void testUserStringStringStringString() {
-		fail("Not yet implemented"); // TODO
+		User tester = new User("kitchen", "tester@gmail.com", "11/04/1997", "testname");
+		
+		assertEquals("kitchen", tester.getRole());
+		assertNotEquals(null, tester.getRole());
+	
 	}
 
 	@Test
 	void testUser() {
-		fail("Not yet implemented"); // TODO
+		User tester = new User();
+		
+		assertNotEquals("kitchen", tester.getRole());
+		assertEquals(null, tester.getRole(), "Should return NULL");
 	}
 
 	@Test
 	void testCreateEmployee() {
-		fail("Not yet implemented"); // TODO
+		User tester = new User();
+		
+		tester.createEmployee("t1", "test name", "kitchen", "1234");
+		
+		assertEquals("kitchen", tester.getRole());
 	}
 
 	@Test
 	void testInitialize() {
-		fail("Not yet implemented"); // TODO
+		//User.initialize(e);
 	}
 
 	@Test
 	void testGetRole() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetRole() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testIsCustomer() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetSession() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetSession() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetEmail() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetEmail() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetPassword() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetPassword() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetId() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetId() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetBirthday() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetBirthday() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetVisits() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetVisits() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testHasBirthdayEntree() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetBirthdayEntree() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testHasFreeSide() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetFreeSide() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetName() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetName() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetPacketDecoder() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetPacketEncoder() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testHasFreeDessert() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetFreeDessert() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetIndex() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetIndex() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testClose() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testGetTableID() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testIsAvailable() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 	@Test
 	void testSetAvailable() {
-		fail("Not yet implemented"); // TODO
+		
 	}
 
 }
