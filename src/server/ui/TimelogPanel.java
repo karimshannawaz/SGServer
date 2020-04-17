6e93be3f51f4e1fd9372b4ec677c817f08302e72
floretakrasniqi <lola13krasniqi@gmail.com>
2020-04-17 17:21:50 -0500

    updates to timelog - floreta

--- src/server/ui/TimelogPanel.java
+++ src/server/ui/TimelogPanel.java
@@ -3,6 +3,9 @@ package server.ui;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.text.DateFormat;
+import java.util.Date;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -14,6 +17,8 @@ public class TimelogPanel extends JPanel {
 	private static final long serialVersionUID = 1644025221735811591L;
 	
 	private JTable table;
+	public JLabel dateLbl;
+	private JLabel lblNewLabel;
 
 	
 	/**
@@ -25,8 +30,13 @@ public class TimelogPanel extends JPanel {
 		setBounds(233, 0, 962, 710);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(198, 68, 572, 575);
+		scrollPane.setBounds(201, 135, 572, 539);
 		add(scrollPane);
+		
+		lblNewLabel = new JLabel("Employee Timelog");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblNewLabel.setBounds(355, 31, 277, 41);
+		add(lblNewLabel);
 		
 		//create table
 		table = new JTable();
@@ -36,6 +46,22 @@ public class TimelogPanel extends JPanel {
 			}, 0
 		));
 		scrollPane.setViewportView(table);
+		
+		//prints current date
+		dateLbl = new JLabel("Current Time: <dynamic date>");
+		dateLbl.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		dateLbl.setBounds(374, 84, 228, 35);
+		add(dateLbl);
+		Date fullDate = new Date();
+		String format = DateFormat.getInstance().format(fullDate);
+		String[] tok = format.split(" ");
+		String date = tok[0];
+		String time = tok[1];
+		String meridiem = tok[2];
+		String[] timeToks = time.split(":");
+		int hour = Integer.parseInt(timeToks[0]);
+		int mins = Integer.parseInt(timeToks[1]);
+		dateLbl.setText("Current Time: " +format);
 		
 	}
 }
