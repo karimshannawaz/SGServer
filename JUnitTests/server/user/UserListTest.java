65ddc056c07564c597386980d7cc3599ce2db81e
ashaw <ashaw@192.168.1.220>
2020-04-17 15:14:38 -0500

    added setup for JUnit tests

---
+++ JUnitTests/server/user/UserListTest.java
@@ -0,0 +1,74 @@
+package server.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserListTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testUserList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddT() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testRemoveT() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testRemoveInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddTInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testIterator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testIncreaseIndex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testDecreaseIndex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testContainsT() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testIndexOf() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
