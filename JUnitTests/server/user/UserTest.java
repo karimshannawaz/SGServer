3d5b2b24ee209c1a6f9d28928ae6493b8c4c98cf
ashaw <ashaw@192.168.1.220>
2020-04-17 18:04:52 -0500

    adding more usertests

--- JUnitTests/server/user/UserTest.java
+++ JUnitTests/server/user/UserTest.java
@@ -14,9 +14,9 @@ class UserTest {
 
 	@Test
 	void testUserStringStringStringString() {
-		User tester = new User("kitchen", "tester@gmail.com", "11/04/1997", "testname");
+		User tester = new User("customer", "tester@gmail.com", "11/04/1997", "testname");
 		
-		assertEquals("kitchen", tester.getRole());
+		assertEquals("customer", tester.getRole());
 		assertNotEquals(null, tester.getRole());
 	
 	}
@@ -45,77 +45,111 @@ class UserTest {
 
 	@Test
 	void testGetRole() {
+		User tester = new User();
 		
+		tester.createEmployee("t1", "test name", "kitchen", "1234");
+		
+		assertEquals("kitchen", tester.getRole());
 	}
 
 	@Test
 	void testSetRole() {
+		User tester = new User();
 		
-	}
-
-	@Test
-	void testIsCustomer() {
-		
-	}
-
-	@Test
-	void testGetSession() {
-		
-	}
-
-	@Test
-	void testSetSession() {
-		
+		tester.setRole("kitchen");
+		assertEquals("kitchen", tester.getRole());
 	}
 
 	@Test
 	void testGetEmail() {
+		User tester = new User("customer", "tester@gmail.com", "11/04/1997", "testname");
 		
+		assertEquals("tester@gmail.com", tester.getEmail());
+		assertNotEquals(null, tester.getEmail());
 	}
 
 	@Test
 	void testSetEmail() {
+		User tester = new User();
+		tester.setEmail("tester@gmail.com");
 		
+		assertEquals("tester@gmail.com", tester.getEmail());
+		assertNotEquals(null, tester.getEmail());
 	}
 
 	@Test
 	void testGetPassword() {
+		User tester = new User();
 		
+		tester.createEmployee("t1", "test name", "kitchen", "1234");
+		
+		assertEquals("1234", tester.getPassword());
 	}
 
 	@Test
 	void testSetPassword() {
+		User tester = new User();
 		
+		tester.setPassword("243");
+		
+		assertEquals("243", tester.getPassword());
 	}
+	
 
 	@Test
 	void testGetId() {
+		User tester = new User();
 		
+		tester.createEmployee("t1", "test name", "kitchen", "1234");
+		
+		assertEquals("t1", tester.getId());
+		assertNotEquals("kitchen", tester.getId());
 	}
 
 	@Test
 	void testSetId() {
+		User tester = new User();
 		
+		tester.setId("t1");
+		
+		assertEquals("t1", tester.getId());
 	}
 
 	@Test
 	void testGetBirthday() {
+		User tester = new User("customer", "tester@gmail.com", "11/04/1997", "testname");
 		
+		assertEquals("11/04/1997", tester.getBirthday());
+		assertNotEquals(null, tester.getBirthday());
 	}
 
 	@Test
 	void testSetBirthday() {
+		User tester = new User("customer", "tester@gmail.com", "11/04/1997", "testname");
 		
+		tester.setBirthday("04/13/20");
+		
+		assertEquals("04/13/20", tester.getBirthday());
+		assertNotEquals(null, tester.getBirthday());
 	}
 
 	@Test
 	void testGetVisits() {
+		User tester = new User("customer", "tester@gmail.com", "11/04/1997", "testname");
 		
+		
+		assertEquals(0, tester.getVisits(), "visits equal 0 at initialization");
+		assertNotEquals(1, tester.getVisits());
 	}
 
 	@Test
 	void testSetVisits() {
+		User tester = new User("customer", "tester@gmail.com", "11/04/1997", "testname");
 		
+		tester.setVisits(5);
+		
+		assertEquals(5, tester.getVisits());
+		assertNotEquals(0, tester.getVisits(), "should no longer equal 0");
 	}
 
 	@Test
