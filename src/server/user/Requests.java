8990130e60c3c55a70cfaa4493d9af61ba9763e2
karimshannawaz <karimshannawaz@my.unt.edu>
2020-04-17 17:08:30 -0500

    added requests

---
+++ src/server/user/Requests.java
@@ -0,0 +1,21 @@
+package server.user;
+
+/**
+ * A class that holds all of the requests.
+ * 
+ * @author Karimshan
+ *
+ */
+public class Requests {
+
+	public static void receiveHelpRequest(int kioskID) {
+		
+	}
+
+	public static void receiveRefillRequest(int kioskID) {
+		
+	}
+	
+	
+
+}
