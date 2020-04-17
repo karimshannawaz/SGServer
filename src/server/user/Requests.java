695a7ef98984811b87b5f4b094105937cddd6f85
karimshannawaz <karimshannawaz@my.unt.edu>
2020-04-17 17:19:08 -0500

    request update

--- src/server/user/Requests.java
+++ src/server/user/Requests.java
@@ -1,5 +1,7 @@
 package server.user;
 
+import server.Global;
+
 /**
  * A class that holds all of the requests.
  * 
@@ -13,16 +15,54 @@ public class Requests {
 	 * @param kioskID
 	 * 
 	 */
-	public static void receiveHelpRequest(int kioskID) {
-		
+	public static void receiveHelpRequest(int tableID) {
+		boolean waitStaffOnline = false;
+		for(User u : Global.getUsers()) {
+			if(u != null) {
+				if(u.getRole().toLowerCase().contains("wait")) {
+					waitStaffOnline = true;
+				}
+			}
+		}
+		if(!waitStaffOnline) {
+			for(User u : Global.getUsers()) {
+				if(u != null) {
+					if(u.getSession().isCustomer()
+						&& u.getTableID() == tableID) {
+						u.getSession().sendClientPacket("waitstaff_not_available_for_request", "help");
+						break;
+					}
+				}
+			}
+			return;
+		}
 	}
 
 	/**
 	 * Receives refill request from client
 	 * @param kioskID
 	 */
-	public static void receiveRefillRequest(int kioskID) {
-		
+	public static void receiveRefillRequest(int tableID) {
+		boolean waitStaffOnline = false;
+		for(User u : Global.getUsers()) {
+			if(u != null) {
+				if(u.getRole().toLowerCase().contains("wait")) {
+					waitStaffOnline = true;
+				}
+			}
+		}
+		if(!waitStaffOnline) {
+			for(User u : Global.getUsers()) {
+				if(u != null) {
+					if(u.getSession().isCustomer()
+						&& u.getTableID() == tableID) {
+						u.getSession().sendClientPacket("waitstaff_not_available_for_request", "refill");
+						break;
+					}
+				}
+			}
+			return;
+		}
 	}
 	
 	
