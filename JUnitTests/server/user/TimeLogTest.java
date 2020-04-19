25320c4e1becd5aa7259e399682462b48289897a
Manasa Nimmagadda <2000manasa13@gmail.com>
2020-04-18 20:22:29 -0500

    junit timelogtest -manasa

---
+++ JUnitTests/server/user/TimeLogTest.java
@@ -0,0 +1,61 @@
+package server.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TimeLogTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		TimeLog tester = new TimeLog();
+	}
+
+	@Test
+	void testTimeLog() {
+		TimeLog tester = new TimeLog();
+		tester.getPunchIns();
+		tester.getPunchOuts();
+		
+		assertEquals(tester.getPunchIns(), tester.getPunchOuts());
+	}
+
+	@Test
+	void testSetId() {
+		TimeLog tester = new TimeLog();
+		tester.setId("mn");
+		assertEquals("mn", tester.getId());
+	}
+
+	@Test
+	void testGetPunchIns() {
+		TimeLog tester = new TimeLog();
+		tester.getPunchIns();
+		
+		assertNotNull(tester.getPunchIns());
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPunchOuts() {
+		TimeLog tester = new TimeLog();
+		tester.getPunchOuts();
+		
+		assertNotNull(tester.getPunchOuts());
+		
+	}
+
+
+	@Test
+	void testGetLog() {
+		TimeLog tester = new TimeLog();
+		tester.getLog("k2");
+		
+		assertNull(tester.getLog("k2"));
+		
+		
+	}
+
+
+}
